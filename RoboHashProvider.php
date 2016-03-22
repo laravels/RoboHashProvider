edb9b08a7a809d99b6fac30705a9e0b1b3a38526
Dirk Diebel <ddiebel@gmx.de>
2016-03-22 13:34:08 +0100

    first commit

---
+++ RoboHashProvider.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Phpmedia\RoboHashProvider;
+
+use Faker\Provider\Base as Base_Provider;
+
+/**
+ * robohash.org provider for Faker.
+ *
+ * @author Dirk Diebel <phpmedia@gmail.com>
+ *
+ * @link http://robohash.org
+ */
+class RoboHashProvider extends Base_Provider
+{
+    const ROBOHASH_URL = 'http://robohash.org/';
+
+    /**
+     * image-url generator to a Robohash roboter avatar
+     *
+     * @param int|string|array $size Size can be an array with width and height or a string like 100x100
+     * @param string $text Optional: Is used to generate the unique avatar.
+     * @param string $format Optional: File-Extension. Possible are jpg, gif and png. Default ist png
+     * @param string $set Optional: Defines the set of roboters to use. Possible are set2 & set3. Default ist set1
+     * @param string $background Optional: Defines a background-set. Possible are bg1 and bg2. Default is null
+     *
+     * @return string
+     */
+    public static function avatarUrl($size, $text = null, $format = 'png', $set = '', $background = '')
+    {
+        $sets = array('', 'set2', 'set3');
+        $backgroundSets = array('', 'bg1', 'bg2');
+        $ext = array('jpg', 'png', 'gif');
+
+        $generatedUrl = self::ROBOHASH_URL;
+
+        /**
+         * set image size
+         */
+        if (is_array($size)) {
+            ksort($size);
+            $size = implode('x', $size);
+        }
+
+        if (!in_array($format, $ext)) {
+            $format = 'png';
+        }
+
+        if (!in_array($set, $sets)) {
+            $set = '';
+        }
+
+        if (!in_array($background, $backgroundSets)) {
+            $background = '';
+        }
+
+        $fileName = ($text != '') ? urlencode($text) : md5(time());
+
+        $urlParams = array('size' => $size);
+
+        if ($set != '') $urlParams['set'] = $set;
+        if ($background != '') $urlParams['bgset'] = $background;
+
+        $generatedUrl .= $fileName . '.' . $format . '?' . http_build_query($urlParams);
+
+        return $generatedUrl;
+    }
+}
---
+++ Tests/RoboHashProviderTest.php
@@ -0,0 +1,72 @@
+<?php
+
+use Phpmedia\RoboHashProvider\RoboHashProvider;
+
+class RoboHashProviderTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @dataProvider roboHashDataProvider
+     */
+    public function testImageUrl($expected, $size, $text = null, $format = 'png', $set = '', $background = '')
+    {
+
+
+        $img = RoboHashProvider::avatarUrl($size, $text, $format, $set, $background);
+
+        $this->assertEquals('http://robohash.org/' . $expected, $img);
+    }
+
+
+    /**
+     * Data provider
+     *
+     * @return array
+     */
+    public function roboHashDataProvider()
+    {
+        return [
+            [
+                'dummy.png?size=100x100',
+                '100x100', 'dummy',
+            ],
+            [
+                'dummy.gif?size=100x100',
+                '100x100', 'dummy', 'gif',
+            ],
+            [
+                'dummy.png?size=100x100',
+                '100x100', 'dummy', 'tif',
+            ],
+            [
+                'dummy.png?size=50x50',
+                '50x50', 'dummy',
+            ],
+            [
+                'dummy.png?size=100x100',
+                array('width' => 100, 'height' => 100), 'dummy',
+            ],
+            [
+                'dummy.png?size=100x100',
+                '100x100', 'dummy', '', 'set1', '',
+            ],
+            [
+                'dummy.png?size=100x100&set=set2',
+                '100x100', 'dummy', '', 'set2', '',
+            ],
+            [
+                'dummy.png?size=100x100&bgset=bg1',
+                '100x100', 'dummy', '', '', 'bg1',
+            ],
+            [
+                'dummy.gif?size=100x100&set=set2&bgset=bg1',
+                '100x100', 'dummy', 'gif', 'set2', 'bg1',
+            ],
+            [
+                'dummy.png?size=200x200&set=set2&bgset=bg1',
+                '200x200', 'dummy', 'png', 'set2', 'bg1',
+            ],
+        ];
+    }
+
+}
